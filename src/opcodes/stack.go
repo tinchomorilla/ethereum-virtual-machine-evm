6be422ef1b82f7d6a99c40781f40a856b15c5d78
tinchomorilla <mmorilla@fi.uba.ar>
2026-04-23 19:23:57 -0300

    implement MakeDup and MakeSwap functions (opcodes) for stack operations

--- src/opcodes/stack.go
+++ src/opcodes/stack.go
@@ -6,6 +6,34 @@ import (
 	"github.com/tinchomorilla/ethereum-virtual-machine-evm/src/types"
 )
 
+// MakeDup returns an OpFunc for DUP_n: duplicates the n-th item from the top.
+// DUP1 (n=1) duplicates the top, DUP16 (n=16) duplicates the 16th item.
+func MakeDup(n int) types.OpFunc {
+	return func(e types.Executor) error {
+		val, err := e.GetStack().Peek(n)
+		if err != nil {
+			return err
+		}
+		if err := e.GetStack().Push(new(big.Int).Set(val)); err != nil {
+			return err
+		}
+		e.SetPC(e.GetPC() + 1)
+		return nil
+	}
+}
+
+// MakeSwap returns an OpFunc for SWAP_n: swaps the top with the (n+1)-th item.
+// SWAP1 (n=1) swaps positions 1 and 2, SWAP16 (n=16) swaps positions 1 and 17.
+func MakeSwap(n int) types.OpFunc {
+	return func(e types.Executor) error {
+		if err := e.GetStack().Swap(n); err != nil {
+			return err
+		}
+		e.SetPC(e.GetPC() + 1)
+		return nil
+	}
+}
+
 func OpPOP(e types.Executor) error {
 	_, err := e.GetStack().Pop()
 	if err != nil {
@@ -37,3 +65,5 @@ func MakePush(n int) types.OpFunc {
 		return e.GetStack().Push(new(big.Int).SetBytes(val))
 	}
 }
+
+
